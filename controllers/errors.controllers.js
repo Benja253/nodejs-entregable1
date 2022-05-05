2762af1bcac9cd00da4b5d81ba0e5132fea08fe4
benjaminfloresm <benja48.9@gmail.com>
2022-05-05 00:20:07 -0400

    Arreglando algunos errores

--- controllers/errors.controllers.js
+++ controllers/errors.controllers.js
@@ -1,4 +1,7 @@
 const globalErrorHandler = (err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const status = err.status || 'fail'
+
   res.status(err.statusCode).json({
     status: 'error',
     error: err,
